2fc7963ddb9cfeaade113078bc0668d7882635e8
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-02-25 15:09:25 -0300

    Entendendo e usando o counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_183704) do
+ActiveRecord::Schema.define(version: 2022_02_25_180210) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_183704) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_counter"
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
