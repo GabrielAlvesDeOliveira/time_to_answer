f9b7edddd0925d437222c7778d28a2a7cac4d17c
Gabriel Alves <alvesg2003@gmail.com>
2022-02-27 23:09:40 -0300

    Adicionando uma Janela Moda

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
   end
   devise_for :admins, skip: [:registrations]
   devise_for :users
+  
   get 'inicio', to: 'site/welcome#index'
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
